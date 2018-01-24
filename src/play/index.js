fefb5bed0f1dee98b65e8dd7ef713ee8ba1d845b
Li Xiao-Bo <xiaoboleee@gmail.com>
2018-01-24 19:03:36 +0800

    open file on local disk

--- src/play/index.js
+++ src/play/index.js
@@ -2,6 +2,11 @@
  * YioLayout
  */
 
+const fs = require('fs');
+const path = require('path');
+const YAML = require('js-yaml');
+const {dialog} = require('electron').remote;
+
 class Topbar extends React.Component {
   render() {
     return React.createElement('div', null, [
@@ -13,9 +18,43 @@ class Topbar extends React.Component {
           key: 'topbar-wrapper',
           className: 'topbar-wrapper'
         }, [
+          React.createElement('input', {
+            key: 'input',
+            type: 'file',
+            ref: 'fileLoadInput',
+            accept: ".yaml, .yml",
+            onChange: e => {
+              var file = e.target.files[0];
+              if (file) {
+                let fileReader = new FileReader()
+
+                fileReader.onload = fileLoadedEvent => {
+                  let textFromFileLoaded = fileLoadedEvent.target.result
+                  this.props.specActions.updateSpec(YAML.safeDump(YAML.safeLoad(textFromFileLoaded)))
+                }
+
+                fileReader.readAsText(file, "UTF-8");
+              }
+            }
+          }),
           React.createElement('span', {
             key: 'name',
-            className: 'topbar-name'
+            className: 'topbar-name',
+            onClick: () => dialog.showOpenDialog({
+              buttonLabel: '打开',
+              properties: ['openFile', 'createDirectory'],
+              filters: [{name: 'yml', extensions: ['yaml']}],
+              message: '打开本地SWAGGER配置文件'
+            }, filePaths => {
+              let file = filePaths[0];
+              if (file) {
+                file = path.resolve(file);
+                fs.readFile(file, (err, data) => {
+                  var code = data.toString();
+                  editor;
+                });
+              }
+            })
           }, 'Swagger Editor')
         ])
       ])
